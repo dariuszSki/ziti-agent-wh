ea1a8a24cc9ccf2a836f212cae054114c8c0aebc
dariuszSki <dariusz.sliwinski@netfoundry.io>
2024-05-06 20:13:47 -0400

    updated delete logic

--- src/webhook.go
+++ src/webhook.go
@@ -10,7 +10,6 @@ import (
 	admissionv1 "k8s.io/api/admission/v1"
 	admissionv1beta1 "k8s.io/api/admission/v1beta1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/klog/v2"
 )
 
@@ -21,8 +20,6 @@ var (
 	sidecarImage  string
 	sidecarName   string
 	runtimeScheme = runtime.NewScheme()
-	codecs        = serializer.NewCodecFactory(runtimeScheme)
-	deserializer  = codecs.UniversalDeserializer()
 )
 
 var CmdWebhook = &cobra.Command{
@@ -42,13 +39,16 @@ func init() {
 		"File containing the default x509 private key matching --tls-cert-file.")
 	CmdWebhook.Flags().IntVar(&port, "port", 9443,
 		"Secure port that the webhook listens on")
-	CmdWebhook.Flags().StringVar(&sidecarImage, "sidecar-image", "openziti/ziti-cli",
+	CmdWebhook.Flags().StringVar(&sidecarImage, "sidecar-image", "openziti/ziti-tunnel",
 		"Image to be used as the injected sidecar")
 	CmdWebhook.Flags().StringVar(&sidecarName, "sidecar-name", "ziti-tunnel",
 		"ContainerName to be used for the injected sidecar")
-	// AdmissionReview is registered for version admission.k8s.io/v1 or admission.k8s.io/v1beta1 in scheme "pkg/runtime/scheme.go:100"
-	_ = admissionv1.AddToScheme(runtimeScheme)
-	_ = admissionv1beta1.AddToScheme(runtimeScheme)
+
+	/*
+		AdmissionReview is registered for version admission.k8s.io/v1 or admission.k8s.io/v1beta1
+		in scheme "https://github.com/kubernetes/apimachinery/blob/master/pkg/runtime/scheme.go:100"
+	*/
+	addToScheme(scheme)
 }
 
 // admitv1beta1Func handles a v1beta1 admission
@@ -78,26 +78,7 @@ func delegateV1beta1AdmitToV1(f admitv1Func) admitv1beta1Func {
 	}
 }
 
-// func createPatch(currentPodSpec metav1.ObjectMeta, newPodSpec corev1.PodSpec) ([]byte, error) {
-// 	// Generate a JSON patch representing the changes to the Pod spec
-// 	patched, err := json.Marshal(newPodSpec)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	// klog.Infof("Generated a JSON patch representing the changes to the Pod spec")
-// 	unpatched, err := json.Marshal(currentPodSpec)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	p, err := jsonpatch.CreatePatch(unpatched, patched)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return json.Marshal(p)
-// }
-
-// serve handles the http portion of a request prior to handing to an admit
-// function
+// serve handles the http portion of a request prior to handing to an admit function
 func serve(w http.ResponseWriter, r *http.Request, admit admitHandler) {
 	var body []byte
 	if r.Body != nil {
@@ -113,7 +94,6 @@ func serve(w http.ResponseWriter, r *http.Request, admit admitHandler) {
 		return
 	}
 
-	// klog.Info(fmt.Sprintf("handling request: %s", body))
 	obj, gvk, err := deserializer.Decode(body, nil, nil)
 	if err != nil {
 		msg := fmt.Sprintf("Request could not be decoded: %v", err)
@@ -159,7 +139,6 @@ func serve(w http.ResponseWriter, r *http.Request, admit admitHandler) {
 		return
 	}
 
-	// klog.Info(fmt.Sprintf("sending response: %v", responseObj))
 	respBytes, err := json.Marshal(responseObj)
 	if err != nil {
 		klog.Error(err)
@@ -172,112 +151,16 @@ func serve(w http.ResponseWriter, r *http.Request, admit admitHandler) {
 	}
 }
 
-// func serve(w http.ResponseWriter, r *http.Request) {
-
-// 	// Initilizes the sidecar container configs
-// 	// sidecarConfig := SidecarConfig{
-// 	// 	Image: "openziti/ziti-tunnel:latest",
-// 	// 	Name:  "zt-tunnel-proxy",
-// 	// }
-
-// 	// Read the AdmissionReview request body
-// 	body, err := io.ReadAll(r.Body)
-// 	if err != nil {
-// 		http.Error(w, "Failed to read request body", http.StatusBadRequest)
-// 		return
-// 	}
-
-// 	// verify the content type is application/json
-// 	contentType := r.Header.Get("Content-Type")
-// 	if contentType != "application/json" {
-// 		klog.Errorf("contentType=%s, expect application/json", contentType)
-// 		return
-// 	}
-
-// 	// Define AdmissionReview struct
-// 	var review admissionv1.AdmissionReview
-
-// 	// Unmarshal the request body into AdmissionReview struct
-// 	if err := json.Unmarshal(body, &review); err != nil {
-// 		http.Error(w, "Failed to unmarshal request body", http.StatusBadRequest)
-// 		return
-// 	}
-
-// 	// Check if request is for Pod creation
-// 	if review.Request.Kind.Kind != "Pod" {
-// 		review.Response = &admissionv1.AdmissionResponse{
-// 			UID:     review.Request.UID,
-// 			Allowed: true,
-// 		}
-// 		response, err := json.Marshal(review)
-// 		if err != nil {
-// 			http.Error(w, "Failed to marshal response", http.StatusInternalServerError)
-// 			return
-// 		}
-
-// 		if _, err := w.Write(response); err != nil {
-// 			http.Error(w, fmt.Sprintf("could not write response: %v", err), http.StatusInternalServerError)
-// 		}
-// 		// klog.Infof(fmt.Sprintf("Request Operation for Returned All except POD: %s", review.Request.Operation))
-// 		// klog.Infof(fmt.Sprintf("Request Operation for Returned All except POD: %s", review.Request.Operation))
-// 		// klog.Infof(fmt.Sprintf("Request Operation for Returned All except POD: %s", review.Request.OldObject))
-// 		return
-// 	}
-
-// 	// Extract the Pod object from the request
-// 	pod := &corev1.Pod{}
-// 	if err := json.Unmarshal(review.Request.Object.Raw, pod); err != nil {
-// 		http.Error(w, "Failed to unmarshal pod object", http.StatusBadRequest)
-// 		return
-// 	}
-
-// 	// Inject the sidecar container into the Pod spec
-// 	// pod.Spec.Containers = append(pod.Spec.Containers, corev1.Container{
-// 	// 	Name:  sidecarConfig.Name,
-// 	// 	Image: sidecarConfig.Image,
-// 	// })
-// 	// klog.Infof("Injected the sidecar container into the Pod spec")
-// 	// Create the AdmissionReview response with the mutated Pod
-// 	// patch, err := createPatch(pod.ObjectMeta, pod.Spec)
-// 	// if err != nil {
-// 	// 	http.Error(w, "Failed to create patch", http.StatusInternalServerError)
-// 	// 	return
-// 	// }
-// 	// klog.Infof("Created the AdmissionReview response with the mutated Pod spec")
-// 	review.Response = &admissionv1.AdmissionResponse{
-// 		UID:     review.Request.UID,
-// 		Allowed: true,
-// 	}
-
-// 	// Marshal the response and send it back to the API server
-// 	response, err := json.Marshal(review)
-// 	if err != nil {
-// 		http.Error(w, "Failed to marshal response", http.StatusInternalServerError)
-// 		return
-// 	}
-
-// 	if _, err := w.Write(response); err != nil {
-// 		http.Error(w, fmt.Sprintf("could not write response: %v", err), http.StatusInternalServerError)
-// 	}
-// 	klog.Infof(fmt.Sprintf("Admission Request - Operation: %s", review.Request.Operation))
-// 	klog.Infof(fmt.Sprintf("Admission Request - Type: %s", pod.Kind))
-// 	klog.Infof(fmt.Sprintf("Admission Request - POD Name: %s", pod.ObjectMeta.Name))
-// 	klog.Infof(fmt.Sprintf("Admission Request - POD Status: %s", pod.Status.Phase))
-// 	for i := 0; i < len(pod.Status.ContainerStatuses); i++ {
-// 		klog.Infof(fmt.Sprintf("Admission Request - Container Name: %s", pod.Status.ContainerStatuses[i].Name))
-// 		klog.Infof(fmt.Sprintf("Admission Request - Container State Running: %s", pod.Status.ContainerStatuses[i].State.Running))
-// 		klog.Infof(fmt.Sprintf("Admission Request - Container State Waiting: %s", pod.Status.ContainerStatuses[i].State.Waiting))
-// 		klog.Infof(fmt.Sprintf("Admission Request - Container State Terminated: %s", pod.Status.ContainerStatuses[i].State.Terminated))
-// 	}
-// 	klog.Info("----------------------------------------------------------------")
-// 	return
-// }
-
-func serveMutatePods(w http.ResponseWriter, r *http.Request) {
-	serve(w, r, newDelegateToV1AdmitHandler(mutatePodsSidecar))
+func serveCreateSC(w http.ResponseWriter, r *http.Request) {
+	serve(w, r, newDelegateToV1AdmitHandler(createSidecar))
 }
-func serveDeletePods(w http.ResponseWriter, r *http.Request) {
-	serve(w, r, newDelegateToV1AdmitHandler(alwaysAllowDelayFiveSeconds))
+
+func serveDeleteSC(w http.ResponseWriter, r *http.Request) {
+	serve(w, r, newDelegateToV1AdmitHandler(deleteSidecar))
+}
+
+func serveUpdateSC(w http.ResponseWriter, r *http.Request) {
+	serve(w, r, newDelegateToV1AdmitHandler(updateSidecar))
 }
 
 func webhook(cmd *cobra.Command, args []string) {
@@ -287,8 +170,9 @@ func webhook(cmd *cobra.Command, args []string) {
 		KeyFile:  keyFile,
 	}
 
-	http.HandleFunc("/mutate-pods", serveMutatePods)
-	http.HandleFunc("/delete-pods", serveDeletePods)
+	http.HandleFunc("/create", serveCreateSC)
+	http.HandleFunc("/delete", serveDeleteSC)
+	http.HandleFunc("/update", serveUpdateSC)
 	server := &http.Server{
 		Addr:      fmt.Sprintf(":%d", port),
 		TLSConfig: configTLS(config),
